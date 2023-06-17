03709bf7c9ac3999540834d2486e946e990ff529
dongjb <dongjb0502@outlook.com>
2023-06-17 10:04:01 +0800

    feat: 部署个人网站

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: '流书随笔',
+  description: '期望在这个繁杂的世界找到一丝心灵的慰藉',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
+  }
+});
---
+++ docs/index.md
@@ -0,0 +1,30 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "流书随笔"
+  text: "生活、运动、学习"
+  tagline: 期望在这个繁杂的世界找到一丝心灵的慰藉
+
+  image:
+    src: /logo.svg
+    alt: logo
+
+  actions:
+    - theme: brand
+      text: 随便走走
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: 快乐生活
+    details: 学会快乐生活，对周围的的世界充满积极乐观的态度，这样才能让你拥有幸福而充满意义的人生。
+  - title: 坚持运动
+    details: 一个坚持运动的人，一定程度上也是热爱生活的人，运动不仅是一种锻炼身体的方式，更是调节情绪的良药。
+  - title: 持续学习
+    details: “活到老，学到老”这句话，从我上学时直到现在，一直在鞭策着我，要成长、要成功，始终离不开学习。我要坚持努力学习，越努力越幸运。
+---
+
