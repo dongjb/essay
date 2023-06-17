109e7073be77ee8e4f268e954ae3c06ddf57f1bd
dongjb <dongjb0502@outlook.com>
2023-06-17 10:34:43 +0800

    chore: 项目前缀

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/essay/',
   title: '流书随笔',
   description: '期望在这个繁杂的世界找到一丝心灵的慰藉',
   themeConfig: {
